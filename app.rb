e04f3dc9c4ee41d41d9495674203f80e2e21df6e
Candice Peters <51180851+cep104@users.noreply.github.com>
2020-11-13 23:48:28 -0600

    Update app.rb
    notes

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require_relative 'config/environment'
 
-class App < Sinatra::Base
-  get '/reverse' do
-    erb :reverse
+class App < Sinatra::Base #inherits from sinatra
+  get '/reverse' do 
+    erb :reverse 
   end
 
   post '/reverse' do
     original_string = params["string"]
     @reversed_string = original_string.reverse
-    erb :reversed
+    erb :reversed #posts to same page as it gets from but erbs are differend so have different views
   end
 
   get '/friends' do
     # Write your code here!
-    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth'] #passing in an array
     erb :friends
   end
 end
